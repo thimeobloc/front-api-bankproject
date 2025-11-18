13f2e0a04b5be2bb0c5a3a707703f8c7a23cbbbd
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-11-18 14:15:18 +0100

    fix bug react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Home from "./pages/Home";
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
-  return  ( <p>hello</p> ) 
+  return  ( 
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+  ) 
 }
