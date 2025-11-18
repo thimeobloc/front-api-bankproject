d2308d7764aaf2996c9912daacbd9b139bd7f899
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-11-18 15:54:54 +0100

    ajout home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Bienvenue sur la page Home !</h1>
+      <h1>Bienvenue sur la page Home !!</h1>
       <p>Ceci est le contenu de la page d'accueil.</p>
     </div>
   );
