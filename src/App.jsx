e358c3aed87691fe5b9ab435a958e7a139fef025
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-11-18 14:15:18 +0100

    ajout d'une page home

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Redirection de la racine "/" vers "/home" */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        {/* Page Home */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur la page Home !</h1>
+      <p>Ceci est le contenu de la page d'accueil.</p>
+    </div>
+  );
+}
+
+export default Home;
