405ab6841edcb3a9f0543cd8dc9452a75cba6df9
Antonin-code <antoninmaubert224@gmail.com>
2025-11-19 12:09:58 +0100

    signin back and front

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,16 +6,18 @@ import { useNavigate } from "react-router-dom";
 export default function Login() {
     const navigate = useNavigate();
     return (
-        <div className="center">
-            <div className="background">
-                <div className="SignForm">
-                    <h1>Connexion</h1>
-                    <input className="input_field" type="text" placeholder="Adresse Email"/>
-                    <input className="input_field"  type="text" placeholder="Mot de passe"/>
+        <section>
+            <div className="center">
+                <div className="background">
+                    <div className="SignForm">
+                        <h1>Connexion</h1>
+                        <input className="input_field" type="text" placeholder="Adresse Email"/>
+                        <input className="input_field"  type="text" placeholder="Mot de passe"/>
 
-                    <button onClick={() => navigate("/Signing")}> Je n'ai pas de compte</button>
+                        <button onClick={() => navigate("/Signing")}> Je n'ai pas de compte</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
 import "../../../src/pages/Beneficiary/Beneficiary.css"
 
 export default function Signing() {
     const navigate = useNavigate();
-    return (
-        <div className="center">
-            <div className="background">
-                <div className="SignForm">
-                <h1>Inscription</h1>
-                <input className="input_field" type="text" placeholder="Nom" />
-                <input className="input_field" type="text" placeholder="Prenom"/>
-                <input className="input_field" type="text" placeholder="Adresse Email"/>
-                <input className="input_field" type="text" placeholder="Mot de passe"/>
-                <input className="input_field" type="text" placeholder="Confirmer le Mot de passe"/>
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-                <button> S'inscrire</button>
-                <button onClick={() => navigate("/Login")}> J'ai déja un compte</button>
+    const handleSubmit = async () => {
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+        }
+        try {
+            const response = await fetch("http://localhost:8000/users/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password
+                }),
+            });
+
+            if (response.ok) {
+                alert("Compte créé avec succès !");
+                navigate("/Login");
+            } else {
+                const data = await response.json();
+                alert("Erreur : " + data.detail);
+            }
+
+        } catch (error) {
+            console.error(error);
+            alert("Impossible de contacter le serveur.")
+        }
+
+    };
+
+    return (
+        <section>
+            <div className="center">
+                <div className="background">
+                    <div className="SignForm">
+                    <h1>Inscription</h1>
+                    <input className="input_field" type="text" placeholder="Nom"
+                           value={name} onChange={(e) => setName(e.target.value)} />
+                    <input className="input_field" type="text" placeholder="Adresse Email"
+                           value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input className="input_field" type="text" placeholder="Mot de passe"
+                           value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input className="input_field" type="text" placeholder="Confirmer le Mot de passe"
+                           value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+
+                    <button onClick={handleSubmit}> S'inscrire</button>
+                    <button onClick={() => navigate("/Login")}> J'ai déja un compte</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
