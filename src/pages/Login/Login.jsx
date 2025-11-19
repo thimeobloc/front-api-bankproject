7f06796b6cd2c77e1a3b72bad110c7ddf1e5a020
Antonin-code <antoninmaubert224@gmail.com>
2025-11-19 09:34:44 +0100

    Fix all css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Login.css';
+import "../../../src/pages/Beneficiary/Beneficiary.css"
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     const navigate = useNavigate();
     return (
-        <div className="background">
-        <div className="SignForm">
-            <h1>Connexion</h1>
-            <input type="text" placeholder="Adresse Email"/>
-            <input type="text" placeholder="Mot de passe"/>
+        <div className="center">
+            <div className="background">
+                <div className="SignForm">
+                    <h1>Connexion</h1>
+                    <input className="input_field" type="text" placeholder="Adresse Email"/>
+                    <input className="input_field"  type="text" placeholder="Mot de passe"/>
 
-            <button onClick={() => navigate("/Signing")}> Je n'ai pas de compte</button>
-        </div>
+                    <button onClick={() => navigate("/Signing")}> Je n'ai pas de compte</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import "../../../src/pages/Beneficiary/Beneficiary.css"
 
 export default function Signing() {
     const navigate = useNavigate();
     return (
-        <div className="background">
-            <div className="SignForm">
-            <h1>Inscription</h1>
-            <input type="text" placeholder="Nom" />
-            <input type="text" placeholder="Prenom"/>
-            <input type="text" placeholder="Adresse Email"/>
-            <input type="text" placeholder="Mot de passe"/>
-            <input type="text" placeholder="Confirmer le Mot de passe"/>
+        <div className="center">
+            <div className="background">
+                <div className="SignForm">
+                <h1>Inscription</h1>
+                <input className="input_field" type="text" placeholder="Nom" />
+                <input className="input_field" type="text" placeholder="Prenom"/>
+                <input className="input_field" type="text" placeholder="Adresse Email"/>
+                <input className="input_field" type="text" placeholder="Mot de passe"/>
+                <input className="input_field" type="text" placeholder="Confirmer le Mot de passe"/>
 
-            <button> S'inscrire</button>
-            <button onClick={() => navigate("/Login")}> J'ai déja un compte</button>
+                <button> S'inscrire</button>
+                <button onClick={() => navigate("/Login")}> J'ai déja un compte</button>
+                </div>
             </div>
         </div>
     );
