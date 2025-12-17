7594bcb5b8533e71edc16368a2ba70365fbe3187
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-12-17 15:28:12 +0100

    change le alt de l'image

--- src/components/beneficiary/BeneficiaryCard.jsx
+++ src/components/beneficiary/BeneficiaryCard.jsx
@@ -4,7 +4,7 @@ import liageImage from './../images/logotransfert.png';
 export default function BeneficiaryCard({ name, iban, imgUrl, onClick }) {
   return (
     <div className="listElement" onClick={onClick} style={{ cursor: "pointer" }}>
-      <img src={liageImage} alt="Image of the account" />
+      <img src={liageImage} alt="logo of the account" />
       <div className="element">
         <h3>{name}</h3>
         <p>{iban}</p>
